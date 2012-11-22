01712e0372347560b07ad5f813fadceb708ee573
Alin Jerpelea <jerpelea@gmail.com>
2012-11-22 12:21:56 +0200

    kumquat: lights changes
    Change-Id: Ifdc86aa8dbb7a4470242e1bf56ba7bdacb71e474

--- include/lights-device.h
+++ include/lights-device.h
@@ -32,18 +32,6 @@ char const*const LED_FILE_TRIGGER[] = {
  "/sys/class/leds/pwr-blue/use_pattern",
 };
 
-char const*const LED_FILE_PATTERN = {
- "/sys/devices/platfrom/nmk-i2c.2/i2c-2/2-0040/pattern_data",
-};
-
-char const*const LED_FILE_DELAYON = {
- "/sys/devices/platfrom/nmk-i2c.2/i2c-2/2-0040/pattern_delay",
-};
-
-char const*const LED_FILE_DELAYOFF = {
- "/sys/devices/platfrom/nmk-i2c.2/i2c-2/2-0040/pattern_duration_secs",
-};
-
 char const*const BUTTON_BACKLIGHT_FILE[] = {
  "/sys/class/leds/l-key-red/brightness",
  "/sys/class/leds/m-key-red/brightness",
@@ -56,4 +44,9 @@ char const*const BUTTON_BACKLIGHT_FILE[] = {
  "/sys/class/leds/r-key-blue/brightness",
 };
 
-char const*const LCD_BACKLIGHT_FILE = "/sys/class/leds/lcd-backlight/brightness";
+char const*const LED_FILE_PATTERN = "/sys/devices/platfrom/nmk-i2c.2/i2c-2/2-0040/pattern_data";
+char const*const LED_FILE_DELAYON = "/sys/devices/platfrom/nmk-i2c.2/i2c-2/2-0040/pattern_delay";
+char const*const LED_FILE_DELAYOFF = "/sys/devices/platfrom/nmk-i2c.2/i2c-2/2-0040/pattern_duration_secs";
+
+char const*const ON = "1";
+char const*const OFF = "0";
