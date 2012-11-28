a93ce464211bbd3d9cdfad4a2a0918063497f074
Keiran Collins <keiran@darkforestgroup.com>
2012-11-28 14:02:20 -0700

    kumquat: fix lights locations
    Change-Id: Ie915beaf4cc14a71714c6329a10b7737cc692a5e
    Signed-off-by: Keiran Collins <keiran@darkforestgroup.com>

--- include/lights-device.h
+++ include/lights-device.h
@@ -44,9 +44,9 @@ char const*const BUTTON_BACKLIGHT_FILE[] = {
  "/sys/class/leds/r-key-blue/brightness",
 };
 
-char const*const LED_FILE_PATTERN = "/sys/devices/platfrom/nmk-i2c.2/i2c-2/2-0040/pattern_data";
-char const*const LED_FILE_DELAYON = "/sys/devices/platfrom/nmk-i2c.2/i2c-2/2-0040/pattern_delay";
-char const*const LED_FILE_DELAYOFF = "/sys/devices/platfrom/nmk-i2c.2/i2c-2/2-0040/pattern_duration_secs";
+char const*const LED_FILE_PATTERN = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/pattern_data";
+char const*const LED_FILE_DELAYON = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/pattern_delay";
+char const*const LED_FILE_DELAYOFF = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/pattern_duration_secs";
 
 char const*const ON = "1";
 char const*const OFF = "0";
