7657dd21e26741cfacfd5bc960de4c36079a6484
Alin Jerpelea <jerpelea@gmail.com>
2013-02-03 08:48:39 +0200

    kumquat: variable notification time
    Change-Id: I5cbe521c3c744494e4b42a70288ed6c337af03a6

--- include/lights-device.h
+++ include/lights-device.h
@@ -38,6 +38,8 @@ char const*const BUTTON_BACKLIGHT_FILE[] = {
 char const*const LED_FILE_PATTERN = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/pattern_data";
 char const*const LED_FILE_DELAYON = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/pattern_delay";
 char const*const LED_FILE_DELAYOFF = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/pattern_duration_secs";
+char const*const LED_FILE_DIMTIME = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/pattern_use_softdim";
+char const*const LED_FILE_DIMONOFF = "/sys/devices/platform/nmk-i2c.2/i2c-2/2-0040/dim_time";
 
 char const*const ON = "1";
 char const*const OFF = "0";
