1777b636fead81ad583c8725b3ce5cb07bd0b1ea
Alin Jerpelea <jerpelea@gmail.com>
2013-05-13 18:48:22 +0300

    kumquat: rename cm_defconfig
    Change-Id: I86777610edc2a36ea53950d361ae86704cd7d828

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,7 @@ USE_CAMERA_STUB := true
 
 TARGET_SPECIFIC_HEADER_PATH += device/sony/kumquat/include
 
-TARGET_KERNEL_CONFIG := cyanogen_kumquat_defconfig
+TARGET_KERNEL_CONFIG := cm_montblanc_kumquat_defconfig
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 16
